582f9411bf8c6c655c12e2985e1d78012a8a9f6d
adil mae <itsadilhadid@gmail.com>
2024-01-04 23:37:10 +0100

    classes

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,32 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(newSize) {
+    this._size = newSize;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(newLocation) {
+    this._location = newLocation;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return this;
+  }
+}
