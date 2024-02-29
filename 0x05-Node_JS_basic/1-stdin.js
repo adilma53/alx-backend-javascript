3dc453da4c189e24f614879dcbaf6294f310e274
adilmae <itsadilhadid@gmail.com>
2024-03-01 00:47:22 +0100

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,15 @@
-const readline = require('readline');
-
-const inputIterface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-console.log('Welcome to Holberton School, what is your name?')
-
-inputIterface.on('line', (answer) => {
-  console.log('Your name is: ' + answer);
-  inputIterface.close();
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
